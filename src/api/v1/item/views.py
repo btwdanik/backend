0c6c9c0050e84494610b40d0d6f38d4c8aae20f3
btwdanik <d4nchzhukov@yandex.ru>
2026-02-13 01:23:28 +0300

    The final lightening of item endpoints, the structure is as light as possible.

--- src/api/v1/item/views.py
+++ src/api/v1/item/views.py
@@ -1,76 +1,64 @@
 from fastapi.responses import JSONResponse
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, Depends
 from typing import List
 
-
-from .dependencies import create_item_user_case
+from .dependencies import create_item_user_case, get_current_user
 from api.pydantic.item.models import ItemSchema, ItemSchemaResponse, Pagination
 from usecase.item.implementation import PostgreSQLCreateItemUC
 
-#TODO: add user | another = Depends(current_active_user) in all endpoints
-
-router = APIRouter(prefix="/items", tags=["Items"])
-
-@router.get("/{item_id}", response_model=ItemSchemaResponse)
-async def get_info_item_by_id(
-        item_id: int,
-        repo: PostgreSQLCreateItemUC = Depends(create_item_user_case)
-    ) -> JSONResponse:
-
-    item = await repo.get(item_id)
-
-    if item is None:
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={})
-    return JSONResponse(status_code=status.HTTP_200_OK, content=item.model_dump())
-
-
-@router.get("", response_model=List[ItemSchemaResponse])
-async def get_info_items(
-        pagination: Pagination = Depends(),
-        repo: PostgreSQLCreateItemUC = Depends(create_item_user_case)
-    ) -> JSONResponse:
-
-    items = await repo.gets(limit=pagination.limit, offset=pagination.offset)
-
-    if items is None:
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=[])
-    return JSONResponse(status_code=status.HTTP_200_OK, content=[item.model_dump() for item in items])
+router = APIRouter(prefix="/users/items", tags=["Items"])
 
 
 @router.post("", response_model=ItemSchemaResponse)
 async def post_item(
         payload: ItemSchema,
+        token: str = Depends(get_current_user),
         repo: PostgreSQLCreateItemUC = Depends(create_item_user_case),
     ) -> JSONResponse:
 
-    item = await repo.create(payload)
+    item = await repo.create(payload, token)
+    return item
 
-    if item is None:
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={})
-    return JSONResponse(status_code=status.HTTP_201_CREATED, content=item.model_dump())
+
+@router.get("/{item_id}", response_model=ItemSchemaResponse)
+async def get_info_item_by_id(
+        item_id: int,
+        token: str = Depends(get_current_user),
+        repo: PostgreSQLCreateItemUC = Depends(create_item_user_case)
+    ) -> JSONResponse:
+
+    item = await repo.get(item_id, token)
+    return item
+
+
+@router.get("", response_model=List[ItemSchemaResponse])
+async def get_info_items(
+        pagination: Pagination = Depends(),
+        token: str = Depends(get_current_user),
+        repo: PostgreSQLCreateItemUC = Depends(create_item_user_case)
+    ) -> JSONResponse:
+
+    items = await repo.gets(limit=pagination.limit, offset=pagination.offset, token=token)
+    return items
 
 
 @router.delete("/{item_id}")
 async def delete_item(
         item_id : int,
+        token: str = Depends(get_current_user),
         repo: PostgreSQLCreateItemUC = Depends(create_item_user_case)
     ) -> JSONResponse:
 
-    result = await repo.delete(item_id)
-
-    if result is None:
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={})
-    return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=result)
+    result = await repo.delete(item_id, token)
+    return result
 
 
 @router.put("/{item_id}", response_model=ItemSchemaResponse)
 async def put_item(item_id : int,
         item: ItemSchema,
+        token: str = Depends(get_current_user),
         repo: PostgreSQLCreateItemUC = Depends(create_item_user_case)
     ) -> JSONResponse:
 
-    item = await repo.update(number=item_id, schema=item)
-
-    if item is None:
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={})
-    return JSONResponse(status_code=status.HTTP_201_CREATED, content=item.model_dump())
+    item = await repo.update(number=item_id, schema=item, token=token)
+    return item
