591f52c58c9ee1114b3dfede1700b6b6f45ccf66
btwdanik <d4nchzhukov@yandex.ru>
2026-02-12 18:17:43 +0300

    Changed endpoints to use accordingly.

--- src/api/v1/user/views.py
+++ src/api/v1/user/views.py
@@ -1,48 +1,42 @@
-from fastapi import APIRouter
 from fastapi.responses import JSONResponse
-from fastapi import APIRouter, status, Depends
-from typing import List
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordRequestForm
 
 from .dependencies import create_user_user_case
-from api.pydantic.user.models import Info, UserSchema, UserSchemaResponse
+from api.pydantic.user.models import UserSchemaResponse, UserSchemaLogin, TokenAccessResponse
 from usecase.user.implementation import PostgreSQLCreateUserUC
 
 from api.pydantic.user.models import UserSchema
-router = APIRouter(prefix="/users")
+router = APIRouter(prefix="/users/auth", tags=["Users"])
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/users/auth/login")
 
-@router.get("/{user_id}", tags=[Info.user_id_info], response_model=UserSchemaResponse)
-async def get_info_user_by_id(
-        user_id: int,
-        repo: PostgreSQLCreateUserUC = Depends(create_user_user_case)
-    ) -> JSONResponse:
-
-    user = await repo.get(user_id)
-
-    if user is None:
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={})
-    return JSONResponse(status_code=status.HTTP_200_OK, content=user.model_dump())
-
-@router.post("", tags=[Info.user_create], response_model=UserSchemaResponse)
-async def post_user(
+@router.post("/register", response_model=UserSchemaResponse)
+async def register_user(
         payload: UserSchema,
         repo: PostgreSQLCreateUserUC = Depends(create_user_user_case),
     ) -> JSONResponse:
 
     user = await repo.create(payload)
+    return user
 
-    if user is None:
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={})
-    return JSONResponse(status_code=status.HTTP_201_CREATED, content=user.model_dump())
+@router.post("/login", response_model=TokenAccessResponse)
+async def login_user(
+        payload: OAuth2PasswordRequestForm = Depends(),
+        repo: PostgreSQLCreateUserUC = Depends(create_user_user_case),
+    ) -> JSONResponse:
+    result = UserSchemaLogin(
+        username=payload.username,
+        password=payload.password,
+    )
+    user = await repo.login(result)
+    return user
 
-
-@router.delete("/{user_id}", tags=[Info.user_delete])
-async def delete_user(
-        user_id: int,
+@router.get("/me", response_model=UserSchemaResponse)
+async def get_info_by_me(
+        token: str = Depends(oauth2_scheme),
         repo: PostgreSQLCreateUserUC = Depends(create_user_user_case)
     ) -> JSONResponse:
 
-    result = await repo.delete(user_id)
-
-    if result is None:
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={})
-    return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=result)
+    user = await repo.get(token)
+    return user
